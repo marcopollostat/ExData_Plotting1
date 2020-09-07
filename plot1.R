dcbb8c0cbac3519d650c64fc0f8058d8f14a629b
marcopollostat <marcopollostat@gmail.com>
2020-09-07 15:05:47 -0300

    Creating code.

--- plot1.R
+++ plot1.R
@@ -1,1 +1,16 @@
+rm(list=ls())
 
+library(dplyr)
+library(data.table)
+dt <- read.delim("~/energy/data/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?" )
+dt$DateTime <- paste(dt$Date, dt$Time)
+dt[['DateTime']] <- strptime(dt[['DateTime']], format = "%d/%m/%Y %H:%M:%S")
+dt$Date <- as.Date(dt$Date, format = "%d/%m/%Y")
+str(dt)
+df <- filter(dt, Date >= "2007-02-01" & Date <= "2007-02-02")
+str(df)
+
+# PLOT 1
+hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+dev.copy(png, file = "plot1.png")#, width=480, height=480)
+dev.off()
