2490e45acd6ca4c300c0b53a103774a376bd9fd9
MarcTraining <marcopollostat@gmail.com>
2020-09-07 16:33:22 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+rm(list=ls()) #clean enviroment
 
 library(dplyr)
 library(data.table)
@@ -6,9 +6,7 @@ dt <- read.delim("~/household_power_consumption.txt", sep = ";", header = TRUE, 
 dt$DateTime <- paste(dt$Date, dt$Time)
 dt[['DateTime']] <- strptime(dt[['DateTime']], format = "%d/%m/%Y %H:%M:%S")
 dt$Date <- as.Date(dt$Date, format = "%d/%m/%Y")
-str(dt)
 df <- filter(dt, Date >= "2007-02-01" & Date <= "2007-02-02")
-str(df)
 
 ##################################################
 # Since I'm at a non-English locale, 
@@ -16,15 +14,7 @@ str(df)
 # where quin = Thu, sex = Fri and sáb = Sat.
 ##################################################
 
+png("plot1.png", width=480, height=480)
 # PLOT 1
 hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
-dev.copy(png, file = "plot1.png")
 dev.off()
-
-#############################################################
-# This plot has a width of 480 pixels
-# and a height of 480 pixels because
-# is a default of the function dev.copy(). 
-# But one can use:
-# dev.copy(png, file = "plot1.png", width=480, height=480)
-#############################################################
